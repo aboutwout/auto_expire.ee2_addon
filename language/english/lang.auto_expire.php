acaa9d5a87a8b92c30479e1ba1efbbf52a73acb9
AboutWout <wouter@baseworks.nl>
2010-05-09 01:10:02 +0200

    Added new language file

---
+++ language/english/lang.auto_expire.php
@@ -0,0 +1,34 @@
+<?php
+  
+$lang = array(
+
+	// SETTINGS
+	'auto_expire_extension_name' =>
+	"Auto Expire",
+
+	'auto_expire_extension_description' =>
+	"Automatically set an entry's expiration date.",
+
+	'heading_preferences' =>
+	"Auto expire settings",
+	
+	'pref_auto_expire' =>
+	"Auto expire entries after",
+
+  'select_period' =>
+  "Select period...",
+  
+  'minutes' => "Minutes",
+  'hours' => "Hours",
+  'days' => "Days",
+  'weeks' => "Weeks",
+  'months' => "Months",
+  'years' => "Years",
+  
+  // END
+  '' => ''
+);
+// end array
+
+/* End of file lang.auto_expire.php */
+/* Location: ./system/language/english/lang.auto_expire.php */
